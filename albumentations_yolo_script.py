4d32b944c74d56b160d14b112cce6a1da40c0555
bahadiryildirimeem <bahadiryildirim15@gmail.com>
2022-08-07 22:39:00 +0300

    Update albumentations_yolo_script.py

--- albumentations_yolo_script.py
+++ albumentations_yolo_script.py
@@ -1,6 +1,6 @@
 import sys
 # if you have some problem with packages edit and uncomment these methods.
-# sys.path.append('c:/users/bahadir/appdata/local/packages/pythonsoftwarefoundation.python.3.10_qbz5n2kfra8p0/localcache/local-packages/python310/site-packages')
+# sys.path.append('c:/users/USR_NAME/appdata/local/packages/pythonsoftwarefoundation.YOUR_PYTHON_DIR/localcache/local-packages/python_YOUR_DIR/site-packages')
 # sys.path.insert(0, "..")
 
 import os
